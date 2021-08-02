93f55e5be7780230ce1cd5ce4f39bc4dab0cc861
Joshus15 <87368782+Joshus15@users.noreply.github.com>
2021-08-02 19:20:12 +0100

    Update shell_script.sh

--- energyproj/shell_script.sh
+++ energyproj/shell_script.sh
@@ -4,5 +4,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 chmod +x shell_script.sh
 python3 -m venv env
 source env/bin/activate
-pip3 install -r requirements.txt
-python3 final_mac.py
+pip3 install -r energyproj/requirements.txt
+python3 energyproj/final_mac.py
